38225287e691cdcaef23004e51d49cd5eb2fd0b2
Diego Chagas <diego.chagas@afya.com.br>
2023-07-03 15:31:43 -0300

    melhorando a responsidade dos cards

--- src/components/Resumo/styles.js
+++ src/components/Resumo/styles.js
@@ -5,16 +5,15 @@ export const Container = styled.div`
   max-width: 70rem;
   margin: 0 auto;
   padding: 0 1.5rem;
-
   display: grid;
-
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
-
   margin-top: -5rem;
+  
 
   @media(max-width: 750px){
-
-   
+    overflow-x:auto;
+    padding: 0 1.5rem 1rem 1rem;
+    gap:1.5rem;
   }
 `;
