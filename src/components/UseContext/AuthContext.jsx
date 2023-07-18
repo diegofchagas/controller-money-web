7cfb17e4debca11b53f8be04c4ff167646600f22
Diego Chagas <diego.chagas@afya.com.br>
2023-07-18 15:23:38 -0300

    as transacoes agora sao salvas no localStorage e foi implementada funcao de apagar transacao selecionada por id

--- src/components/UseContext/AuthContext.jsx
+++ src/components/UseContext/AuthContext.jsx
@@ -3,19 +3,27 @@ import React , {createContext, useState} from 'react'
 export const AuthContext = createContext()
 
 const UseProvider = ({children}) => {
-  const [transacoes, setTransacoes ] = useState([])
-  const [filtro, setFiltro] = useState([])
+    const [transacoes, setTransacoes ] = useState(()=> {
+    const  storedTransactions = localStorage.getItem('transactions');
+    return storedTransactions ? JSON.parse(storedTransactions) : [];
+  });
+  const [filtro, setFiltro] = useState([]);
 
 
   const criarTransacoes = (transacao) => {
     setTransacoes([...transacoes, transacao])
   }
 
+  const deleteTransacoes = (id) =>{
+    const novasTransacoes = transacoes.filter((transacao) => transacao.id !== id);
+    setTransacoes(novasTransacoes);
+  }
+
   const filtrarTransacoes = (transacoesFiltradas) => {
     setFiltro(transacoesFiltradas)
   }
   return (
-    <AuthContext.Provider value={{transacoes, criarTransacoes, filtro, filtrarTransacoes}}>
+    <AuthContext.Provider value={{transacoes, criarTransacoes, filtro, filtrarTransacoes, deleteTransacoes}}>
       {children}
     </AuthContext.Provider>
   )
